2441a4853661a311952499981a382f5681f222e8
Ritesh2k24 <pawar.ritesh2018@gmail.com>
2025-04-22 10:26:45 +0000

    terraform files are added new

---
+++ Terraform/main.tf
@@ -0,0 +1,57 @@
+provider "aws" {
+  region  = "us-east-1"
+  profile = "default"
+}
+
+resource "aws_instance" "strapi_ec2" {
+  ami                    = "ami-08a52ddb321b32a8c" 
+  instance_type          = "t2.small"
+  key_name               = "master"
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt update -y
+              apt install -y docker.io
+              systemctl start docker
+              systemctl enable docker
+              docker run -d -p 1337:1337 (var.image_uri)
+              EOF
+
+  tags = {
+    Name = "Strapi-Docker-Terraform"
+  }
+
+  vpc_security_group_ids = [aws_security_group.strapi_sg.id]
+}
+
+resource "aws_security_group" "strapi_sg" {
+  name        = "strapi_sg"
+  description = "Allow SSH and Strapi port"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 1337
+    to_port     = 1337
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
